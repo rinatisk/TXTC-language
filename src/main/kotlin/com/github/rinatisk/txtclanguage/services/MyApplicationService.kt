69a783c6e928169fb9ce42d62a976df6ee576e4c
GitHub Action <action@github.com>
2021-04-30 20:48:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/rinatisk/txtclanguage/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rinatisk/txtclanguage/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rinatisk.txtclanguage.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rinatisk.txtclanguage.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/rinatisk/txtclanguage/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rinatisk/txtclanguage/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rinatisk.txtclanguage.services
 
+import com.github.rinatisk.txtclanguage.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
