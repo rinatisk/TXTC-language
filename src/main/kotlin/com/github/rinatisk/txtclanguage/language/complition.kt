794451ca6e55cef4bb507059b3c5552a48188e01
rinatisk <st087501@student.spbu.ru>
2021-05-03 19:30:47 +0300

    first working version

--- src/main/kotlin/com/github/rinatisk/txtclanguage/language/complition.kt
+++ src/main/kotlin/com/github/rinatisk/txtclanguage/language/complition.kt
@@ -19,9 +19,10 @@ class SimpleCompletionContributor : CompletionContributor() {
                      context: ProcessingContext,
                      result: CompletionResultSet
                 ) {
+                    println(parameters.position.text.dropLast(20))
                   //  setOf(File("/usr/share/dict/american-english").readLines().filter { it.startsWith("He") }))
-                    val words = File("/usr/share/dict/american-english").readLines().filter { (it.startsWith(parameters.position.text)) }
-
+                    val words = File("/usr/share/dict/american-english").readLines().filter { (it.startsWith(parameters.position.text.dropLast(20))) }
+                  //  result.addElement(LookupElementBuilder.create(parameters.position.text.dropLast(20)))
                     result.addAllElements(words.map { LookupElementBuilder.create(it) } )
 
 
